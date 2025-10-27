b16580fc8850f523edbf9f252872f9b9b35719d6
sajibkhandev <sajibkhan.mern@gmail.com>
2025-10-27 21:43:21 +0600

    set middlewar

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,8 +1,93 @@
 import React from 'react'
+import { Button, Checkbox, Form, Input } from 'antd';
+import axios from 'axios'
 
 const App = () => {
+
+  const onFinish =async values => {
+
+   let data= await axios.post('http://localhost:3000/api/v1/authentication/registration',{
+      username:values.username,
+      email:values.email,
+      password:values.password
+
+    },
+    {
+      headers:{
+        Authorization:"ertegdfbrtyrd2345"
+      }
+    }
+  
+  )
+
+    console.log(data);
+    
+
+    
+    
+
+
+
+ 
+};
+const onFinishFailed = errorInfo => {
+  console.log('Failed:', errorInfo);
+};
+
+
+
+
+
+
   return (
-    <div><h1>This is Dashboard</h1></div>
+     <Form
+    name="basic"
+    labelCol={{ span: 8 }}
+    wrapperCol={{ span: 16 }}
+    style={{ maxWidth: 600 }}
+    initialValues={{ remember: true }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+  >
+    <Form.Item
+      label="Username"
+      name="username"
+      rules={[{ required: true, message: 'Please input your username!' }]}
+    >
+      <Input />
+    </Form.Item>
+
+
+
+    <Form.Item
+      label="Email"
+      name="email"
+      rules={[{ required: true, message: 'Please input your Email!' }]}
+    >
+      <Input />
+    </Form.Item>
+
+
+
+
+
+    <Form.Item
+      label="Password"
+      name="password"
+      rules={[{ required: true, message: 'Please input your password!' }]}
+    >
+      <Input.Password />
+    </Form.Item>
+
+   
+
+    <Form.Item label={null}>
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form>
   )
 }
 
